e5f32d86bea0e78e353861520ca1f04f5fd43f94
xiaoxiae <tomas.slama.131@gmail.com>
2020-03-29 10:59:16 +0200

    add algorithms.py (skeleton)

---
+++ src/algorithms.py
@@ -0,0 +1,7 @@
+"""This class contains examples of how to use the app to visualize algorithms."""
+
+# TODO: ideas for algorithms
+# - BFS, DFS
+# - dijkstra
+# - graph from score
+# - strongly and weakly connected components
