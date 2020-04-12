f870a4afbb8d9939b957c1dcb123c87f7a7053ad
xiaoxiae <tomas.slama.131@gmail.com>
2020-04-12 11:58:50 +0200

    some algorithms.py ideas

--- src/algorithms.py
+++ src/algorithms.py
@@ -5,3 +5,12 @@
 # - dijkstra
 # - graph from score
 # - strongly and weakly connected components
+
+from graph import Graph
+
+
+def bfs(graph: Graph):
+    """A BFS implementation."""
+
+    # asserts for the algorithm to function properly
+    assert not graph.get_weighted(), "Graph mustn't be weighted."
